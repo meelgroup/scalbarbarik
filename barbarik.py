1ef931db5fbb99a87e3488c89a7c7c730c6d8603
Mate Soos <soos.mate@gmail.com>
2020-01-19 17:12:34 +0100

    Adding pylint disabling of issues

--- barbarik.py
+++ barbarik.py
@@ -18,6 +18,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301, USA.
 
+# pylint: disable=invalid-name,line-too-long,too-many-locals
+
 from __future__ import print_function
 import sys
 import os
